cd2eed80ae6d6017a1eb8a119922486b88035c88
Niels G. W. Serup <ngws@metanohi.name>
2025-01-12 16:14:42 +0100

    Ensure CSRF cookie presence without form

--- varer/urls.py
+++ varer/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 from . import views
 
 app_name = "varer"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+    path("", ensure_csrf_cookie(views.IndexView.as_view()), name="index"),
     path("upload/<filename>", views.upload, name="upload"),
     path("upload/<filename>/thumbnail", views.upload_thumbnail, name="upload_thumbnail"),
     path("varer/ny/", views.ny, name="ny"),
