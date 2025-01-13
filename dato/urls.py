ed3ac45ab00e901112674ee6c89db73dc752577a
Niels G. W. Serup <ngws@metanohi.name>
2025-01-13 21:45:40 +0100

    Make everything work for local development

--- dato/urls.py
+++ dato/urls.py
@@ -16,8 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('varer.urls')),
     path('admin/', admin.site.urls),
+
+    # Only for local development
+    re_path(
+        r"^static/(?P<path>.*)$",
+        serve,
+        {
+            "document_root": settings.STATIC_ROOT,
+        },
+        )
 ]
--- varer/urls.py
+++ varer/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = "varer"
 urlpatterns = [
     path("", ensure_csrf_cookie(views.IndexView.as_view()), name="index"),
-    path("upload/<filename>", views.upload, name="upload"),
+    path("upload/<filename>", views.upload, name="upload"), # Only for local development
     path("upload/<filename>/thumbnail", views.upload_thumbnail, name="upload_thumbnail"),
     path("varer/ny/", views.ny, name="ny"),
     path("varer/ny/opret/", views.ny_opret, name="ny_opret"),
